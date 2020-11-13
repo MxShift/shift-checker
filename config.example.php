959571ddcb71f53dadc2c94d80ae1f1dae6a60fd
Mx <mx@etlgr.com>
2020-11-13 11:22:56 +0000

    docs(config.example.php): fix some typos + description

--- config.example.php
+++ config.example.php
@@ -1,5 +1,5 @@
 <?php
-	/**
+    /**
      * @version 2.1.2
      * @author Mx
      * @link https://github.com/MxShift/shift-checker
@@ -16,7 +16,7 @@ __________________________ */
     $nodeName       = "Your Node Name"." on ".gethostname();    // Name of this node delegate for Telegram messages.
 
 // Consensus settings
-    $consensusEnable    = true;                  // true is for enable consensus check. Be sure to add nodes addresses first
+    $consensusEnable    = true;                  // Set true for enable consensus check. Be sure to add nodes addresses first
     $main               = true;                  // Is this your main node? true/false
     $mainnode           = "http://NODE_IP";      // Main node IP address. Use node IP address or http://127.0.0.1 if it's a local node
     $mainport           = 9405;                  // Main node port
@@ -26,18 +26,18 @@ __________________________ */
 
 // Recovery settings
     $restoreEnable  = true; 
-	$apiHost        = "http://127.0.0.1:9405";		        // Used to calculate $publicKey by $secret for consensus check and to check syncing. Use http://127.0.0.1:netPort    
-    $explorer	    = "https://testnet.shiftnrg.com.mx"; 	// Used to check blockchain height
+    $apiHost        = "http://127.0.0.1:9405";                // Used for calculating $publicKey by $secret for consensus check and to check syncing. Use http://127.0.0.1:netPort or https://127.0.0.1:netPort if you enabled SSL
+    $explorer        = "https://testnet.shiftnrg.com.mx";     // Used for checking blockchain height. Replace it to mainnet or testnet explorer
 
 // Snapshot settings
-    $createsnapshot	= true;					// Do you want to create snapshots with shift-snapshot?
-    $max_snapshots	= 2;					// How many snapshots to preserve? (in days)
+    $createsnapshot    = true;                    // Do you want to create snapshots with shift-snapshot?
+    $max_snapshots    = 2;                        // How many snapshots to preserve? (in days)
 
 // Telegram Bot
-    $telegramAll	= false;				// Change to false to disable all messages exept recovery messages from Telegram bot
-    $SyncingMessage = true;					// Change to true if you want recieve messagese of your recovery status
-    $telegramId 	= "here"; 				// Your Telegram ID
-    $telegramApiKey = "here"; 				// Your Telegram API key 
+    $telegramAll    = false;                   // Change it to false to disable all messages exept recovery messages from Telegram bot
+    $SyncingMessage = true;                    // Change it to true if you want to recieve messagese of your recovery status
+    $telegramId     = "here";                  // Your Telegram ID
+    $telegramApiKey = "here";                  // Your Telegram API key
 
 
 /*  GENERAL CONFIG
@@ -49,30 +49,30 @@ __________________________ */
 
 // You may leave the settings below as they are
 
-	$date		= date("Y-m-d H:i:s");			    // Current date
-	$pathtoapp	= $homeDir."shift/";			    // Full path to your shift installation	
-	$baseDir	= dirname(__FILE__)."/";		    // Folder which contains THIS file
-	$lockfile	= $baseDir."checkdelegate.lock";	// Name of our lock file
-    $database	= $baseDir."db.json";	            // Database name to use
-	$msg 		= "\"cause\":3";			        // Message that is printed when forked
-	$shiftlog 	= $pathtoapp."logs/shift.log";		// Needs to be a FULL path, so not ~/shift
-	$linestoread	= 30;					        // How many lines to read from the end of $shiftlog
-	$max_count 	= 3;					            // How may times $msg may occur
-	$okayMsg 	= "√";					            // 'Okay' message from shift_manager.bash
+    $date        = date("Y-m-d H:i:s");                // Current date
+    $pathtoapp    = $homeDir."shift/";                 // Full path to your shift installation    
+    $baseDir    = dirname(__FILE__)."/";               // Folder which contains THIS file
+    $lockfile    = $baseDir."checkdelegate.lock";      // Name of our lock file
+    $database    = $baseDir."db.json";                 // Database name to use
+    $msg         = "\"cause\":3";                      // Message that is printed when forked
+    $shiftlog     = $pathtoapp."logs/shift.log";       // Needs to be a FULL path, so not ~/shift
+    $linestoread    = 30;                              // How many lines to read from the end of $shiftlog
+    $max_count     = 3;                                // How may times $msg may occur
+    $okayMsg     = "√";                                // 'Okay' message from shift_manager.bash
 
 // Consensus settings
-	$threshold      = 20;                           // Percentage of consensus threshold.
+    $threshold      = 20;                            // Percentage of consensus threshold.
 
 // Syncing settings
-	$restoredMsg 	= "OK snapshot restored successfully.";	    // 'Okay' message from shift-snapshot	 	
-	$snapThreshold  = 3200;                 		            // Threshold in blocks. Use 3200 for daily snapshots and 133 for hourly
+    $restoredMsg     = "OK snapshot restored successfully.";        // 'Okay' message from shift-snapshot         
+    $snapThreshold  = 3200;                                         // Threshold in blocks. Use 3200 for daily snapshots and 133 for hourly
 
 // Snapshot settings
-	$snapshotDir	= $homeDir."shift-snapshot/";	// Base folder of shift-snapshot
+    $snapshotDir    = $homeDir."shift-snapshot/";    // Base folder of shift-snapshot
 
 // Log file rotation
-	$logfile 	= $baseDir."logs/checkdelegate.log";            // The location of your log file (see section crontab on Github)
-	$max_logfiles	= 3;					        // How many log files to preserve? (in days)  
-	$logsize 	= 5242880;				            // Max file size, default is 5 MB
+    $logfile     = $baseDir."logs/checkdelegate.log";         // The location of your log file (see section crontab on Github)
+    $max_logfiles    = 3;                                     // How many log files to preserve? (in days)  
+    $logsize     = 5242880;                                   // Max file size, default is 5 MB
 
 ?>
