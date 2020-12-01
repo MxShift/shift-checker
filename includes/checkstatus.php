9580800047ec6ed6006e9949f4684f3431534827
MxShift <mx@etlgr.com>
2020-12-01 12:46:19 +0000

    fix(checkstatus): add pause to wait API after start
    + refactor with new functions + text changes

--- includes/checkstatus.php
+++ includes/checkstatus.php
@@ -1,28 +1,29 @@
 <?php
 
 echo "\n[ STATUS ]\n\n";
-echo "\t\t\tLet's check if our delegate is still running... ";
+echo "\t\t\tLet's check if our node is still running: ";
 
 // Check status with shift_manager.bash. Use PHP's ob_ function to create an output buffer
-  ob_start();
-  $check_status = passthru("cd $pathtoapp && bash shift_manager.bash status | cut -z -b1-3");
-  $check_output = ob_get_contents();
-  ob_end_clean();
+ob_start();
+$check_status = passthru("cd $pathtoapp && bash shift_manager.bash status | cut -z -b1-3");
+$check_output = ob_get_contents();
+ob_end_clean();
 
 // Somewhere here we need to add a check for manual rebuild by the user
 
 // If status is not OK...
-  if (strpos($check_output, $okayMsg) === false) {
-      
+if (strpos($check_output, $okayMsg) === false) {
+    
     // Echo something to our log file
-      $Tmsg = "Delegate ".$nodeName." not running/healthy. Restarting Shift..";
-      sendMessage($Tmsg);
+    $Tmsg = "Delegate ".$nodeName." not running. Restarting Shift";
+    sendMessage($Tmsg);
 
-      echo "\n\t\t\t".$Tmsg."\n";
-      //Restarting Shift
-      passthru("cd $pathtoapp && bash shift_manager.bash reload");
-   
-  // If status is OK
-  } else {
-      echo "YES!\n";
-  }
+    echo "\n\t\t\t".$Tmsg."\n";
+    //Restarting Shift
+    shiftManager("reload");
+    pauseToWaitNodeAPI(20);
+
+// If status is OK
+} else {
+    echo "YES!\n";
+}
