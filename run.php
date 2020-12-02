6c670d98f7a6633eac1cd6ebf1528f018d661380
Mx <main@vmi483557.contaboserver.net>
2020-12-02 12:09:36 +0100

    refactor(checkdelegate): rename to run.php

--- run.php
+++ run.php
@@ -1,14 +1,4 @@
 <?php
-  /**
-   * @version 2.1.2
-   * @author Mx
-   * @link https://github.com/MxShift/shift-checker
-   * @license https://github.com/MxShift/shift-checker/blob/master/LICENSE
-   * @version 2.0
-   * @author Jan
-   * @link https://github.com/lepetitjan/shift-checker
-   * @license https://github.com/lepetitjan/shift-checker/blob/master/LICENSE
-   */
 
 /* PREREQUISITES
 ____________________ */
