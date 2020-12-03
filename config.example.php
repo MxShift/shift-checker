483ba54f8326a301925e608ad669fb8092718e32
MxShift <mxshifter@gmail.com>
2020-12-03 20:36:53 +0100

    refactor(config): reame nodes to local and remote

--- config.example.php
+++ config.example.php
@@ -7,14 +7,13 @@ __________________________ */
 
 // Consensus settings
     $consensusEnable    = true;                       // Set true for enable consensus check. Be sure to add nodes addresses first
-    $main               = true;                       // Is this your main node? true/false
-    $mainnode           = "http://127.0.0.1:9305";    // Main node IP address. Use node IP address:port or http://127.0.0.1:9305 if it's a local node
-    $backupnode         = "http://NODE_IP:9305";      // Backup node IP address. Use node IP address:port or http://127.0.0.1:9305 if it's a local node
+    $main               = true;                       // 'true' if this is your main node, 'false' if it's your backup node
+    $localNode          = "http://127.0.0.1:9305";    // Local node IP address. Use http://127.0.0.1:PORT
+    $remoteNode         = "http://NODE_IP:9305";      // Remote node IP address. Use node IP address:port
     $secret             = "passphrase";               // Your twelve word passphrase is placed here. Required for consensus check
 
 // Recovery settings
-    $recoveryEnabled    = true; 
-    $apiHost            = "http://127.0.0.1:9305";                // Used for calculating $publicKey by $secret for consensus check and to check syncing. Use http://127.0.0.1:netPort or https://127.0.0.1:netPort if you enabled SSL
+    $recoveryEnabled    = true;
     $trustedNode        = "https://wallet.shiftnrg.org";          // Used for checking blockchain height. Replace it to mainnet or testnet trusted node
 
 // Snapshot settings
@@ -35,8 +34,9 @@ __________________________ */
 
 // You should have installed shift-checker as a normal user, so the line below should work by default.
 // However, if you installed as root (please don't..) change the path below to $homeDir = "/root/";
-$homeDir            = "/home/".get_current_user()."/";
-$pathtoapp          = $homeDir."shift-lisk/";             // Full path to your shift installation    
-$baseDir            = dirname(__FILE__)."/";              // Folder which contains THIS file
+    $homeDir            = "/home/".get_current_user()."/";
+    $pathtoapp          = $homeDir."shift-lisk/";             // Full path to your shift installation    
+    $baseDir            = dirname(__FILE__)."/";              // Folder which contains THIS file
+    $date               = date("Y-m-d H:i:s");                // Current date
 
 ?>
