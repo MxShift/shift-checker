68081ff54eb3da47b999c1355e2ff49307a72c3d
MxShift <mxshifter@gmail.com>
2020-12-09 22:07:40 +0100

    :white_check_mark: test(test-config): add check for a network of the directory

--- test-config.php
+++ test-config.php
@@ -14,6 +14,7 @@ $endColor = "\033[0m";
 $green = "\e[32m";
 
 $shiftDir = $pathtoapp;
+// $shiftDir = $homeDir."shift-lisk/";
 $localRole = (($thisMain) ? "MAIN" : "BACKUP");
 $remoteNode = (($switchingEnabled) ? $remoteNode : "SWITCHING DISABLED");
 $localNetwork = ((strpos($localNode, "9305")) ? "MAINNET" : "TESTNET");
@@ -23,20 +24,24 @@ $remoteAPI = ((ping($remoteNode)) ? "AVAIBLE" : "INACCESSIBLE");
 $localForgingAPI = ((checkForging($localNode, $public) == "true" || (checkForging($localNode, $public) == "false")) ? "AVAIBLE" : (((checkForging($localNode, $public) == "error") ? "INACCESSIBLE" : "NO SECRET")));
 $remoteForgingAPI = ((checkForging($remoteNode, $public) == "true" || (checkForging($remoteNode, $public) == "false")) ? "AVAIBLE" : (((checkForging($remoteNode, $public) == "error") ? "INACCESSIBLE" : "NO SECRET")));
 $trustedNode = (($recoveryEnabled) ? $trustedNode : "REECOVERY DISABLED");
+$shiftDirNetwork = ((strpos(tailCustom($shiftDir."config.json", 500), "9305")) ? "MAINNET" : ((strpos(tailCustom($shiftDir."config.json", 500), "9405")) ? "TESTNET" : "NO SHIFT-LISK INSTALLATION"));
 
-echo "shift-lisk directory: \t $shiftDir \n";
-echo "\n";
+
 echo "This node is: \t\t $localRole \n";
 echo "Remote node: \t\t $remoteNode \n";
 echo "\n";
-echo "This node network: \t $localNetwork \n";
+echo "shift-lisk directory: \t $shiftDir \n";
+echo "\n";
+echo "Directory network: \t $shiftDirNetwork \n";
+echo "Local node network: \t $localNetwork \n";
 echo "Remote node network: \t $remoteNetwork \n";
 echo "\n";
-echo "This node API: \t\t $localAPI \n";
+echo "Local node API: \t $localAPI \n";
 echo "Remote node API: \t $remoteAPI \n";
 echo "\n";
-echo "This forging API: \t $localForgingAPI \n";
+echo "Local forging API: \t $localForgingAPI \n";
 echo "Remote forging API: \t $remoteForgingAPI \n";
 echo "\n";
 echo "Trusted node: \t\t $trustedNode \n";
+
 ?>
