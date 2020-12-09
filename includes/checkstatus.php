e8c0db33a31e367f613b6a8a395b10b6b259aaa0
MxShift <mxshifter@gmail.com>
2020-12-09 12:00:42 +0100

    :recycle: refactor(checkstatus): dry with a function

--- includes/checkstatus.php
+++ includes/checkstatus.php
@@ -3,18 +3,15 @@
 echo "\n[ STATUS ]\n\n";
 echo "\t\t\tLet's check if our node is still running: ";
 
-// Check status with shift_manager.bash. Use PHP's ob_ function to create an output buffer
-ob_start();
-$check_status = passthru("cd $pathtoapp && bash shift_manager.bash status | cut -z -b1-3");
-$check_output = ob_get_contents();
-ob_end_clean();
+// Check status with shift_manager.bash
+$output = shiftManager("status");
 
 // Somewhere here we need to add a check for manual rebuild by the user
 
 $apiIsDown = !ping($localNode);
 
 // If status is not OK...
-if (strpos($check_output, $okayMsg) === false || $apiIsDown) {
+if (strpos($output, $okayMsg) === false || $apiIsDown) {
 
     echo "NO!\n";
     
