1276c5749aaaedc473aef718915e23a804cb3e22
MxShift <mxshifter@gmail.com>
2020-12-16 23:10:05 +0100

    :recycle: refactor(cleanup): add unlock function

--- includes/cleanup.php
+++ includes/cleanup.php
@@ -5,6 +5,4 @@ echo "\t\t\tPerforming log rotation and cleanup...\n";
 rotateLog($logfile, $max_logfiles, $logsize);
 
 // Remove lock file
-if (!unlink($lockfile)) {
-    echo "[ LOCKFILE ] Unable to remove lock file!\n";
-}
+unlockScript();
